235dd79c541f9c848664ad586d64cc2ff5a82738
BlackHigh <Innocent1150@gmail.com>
2024-10-07 09:15:22 +0700

    update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenu} from '@angular/material/menu'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatIconModule,MatMenu,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
+
 })
+
 export class AppComponent {
   title = 'klung-ya-web-angular-app';
 }
